32040b65c78ea1051eb4664e21a1d7f97f15dcdc
Crowdbotics <team@crowdbotics.com>
2024-05-01 03:37:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "brendas-world-47758",
+  url: "https://brendas-world-47758.botics.co",
+  api: "https://brendas-world-47758.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
